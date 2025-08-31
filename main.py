eaf57f437687c8f4b4966231cc95c7b9072ac814
Space <ashrayvarughese@gmail.com>
2025-08-31 21:42:14 +0530

    all the nessary things are in this commit

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
 pygame.init()
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen_width = 1000
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 pygame.display.set_caption("Wpm test")
+clock = pygame.time.Clock()
+delta = 0.1
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.fill((255, 255, 255))
+    
+    
+    
+    pygame.display.flip()    
+    clock.tick(60)
